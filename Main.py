81fe4854b20e5d6c176229166284817ac50e9c5f
dryladen <delfanladen24@gmail.com>
2021-09-03 18:58:41 +0800

    Add comments

--- Main.py
+++ Main.py
@@ -1,15 +1,14 @@
-import cv2 as cv  # library untuk image prossecing
+import cv2 as cv  # library untuk image processing
 import numpy as np
 
 
 def KonversiWarna(foto, warna, judul):
     banding = np.hstack((foto, warna))
-    cv.imshow(judul, banding)
-    cv.waitKey()
+    cv.imshow(judul, banding)  # untuk menampilkan foto ke layar
+    cv.waitKey()  # menunggu inputan untuk menutup windows foto
 
 
 def menuKonversiRuangWarna(foto):
-
     print("""
     1. RGB ke HLS
     2. RGB ke HSV
@@ -19,18 +18,23 @@ def menuKonversiRuangWarna(foto):
     """)
     menuKonversi = input("Masukan pilihan : ")
     if(menuKonversi == "1"):
+        # untuk konversi ruang warna
         warna = cv.cvtColor(foto, cv.COLOR_RGB2HLS)
         KonversiWarna(foto, warna, "RGB ke HLS")
     elif(menuKonversi == "2"):
+        # untuk konversi ruang warna
         warna = cv.cvtColor(foto, cv.COLOR_RGB2HSV)
         KonversiWarna(foto, warna, "RGB ke HSV")
     elif(menuKonversi == "3"):
+        # untuk konversi ruang warna
         warna = cv.cvtColor(foto, cv.COLOR_RGB2YCrCb)
         KonversiWarna(foto, warna, "RGB ke YCrCb")
     elif(menuKonversi == "4"):
+        # untuk konversi ruang warna
         warna = cv.cvtColor(foto, cv.COLOR_RGB2LUV)
         KonversiWarna(foto, warna, "RGB ke LUV")
     elif(menuKonversi == "5"):
+        # untuk konversi ruang warna
         warna = cv.cvtColor(foto, cv.COLOR_RGB2LAB)
         KonversiWarna(foto, warna, "RGB ke LAB")
     else:
@@ -38,8 +42,8 @@ def menuKonversiRuangWarna(foto):
 
 
 fotoBunga = cv.imread("Foto bunga.jpeg")
-dimension = (369, 492)
-resized = cv.resize(fotoBunga, dimension)
+dimension = (369, 492)  # ukuran foto yang baru
+foto = cv.resize(fotoBunga, dimension)  # untuk mengatur ukuran foto
 while(True):
     print("="*64)
     print(">> Program Konversi Ruang Warna")
@@ -52,9 +56,9 @@ while(True):
     print("="*64)
     menuUtama = input("Masukan pilihan : ")
     if(menuUtama == "1"):
-        cv.imshow("Foto Asli", resized)
-        cv.waitKey()
+        cv.imshow("Foto Asli", foto)  # untuk menampilkan foto ke layar
+        cv.waitKey()  # menunggu inputan untuk menutup windows foto
     elif(menuUtama == "2"):
-        menuKonversiRuangWarna(resized)
+        menuKonversiRuangWarna(foto)
     elif(menuUtama == "3"):
         break
