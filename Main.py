53a12f171393dca60bc2dfa45e98f725103cd6c7
dryladen <delfanladen24@gmail.com>
2021-09-12 13:40:57 +0800

    Update function menuPilihFoto()

--- Main.py
+++ Main.py
@@ -11,6 +11,7 @@ def KonversiWarna(foto, warna, judul):
 
 
 def menuKonversiRuangWarna(foto):
+
     print("""
     1. RGB ke HLS
     2. RGB ke HSV
@@ -54,30 +55,36 @@ def menuPilihFoto(foto1, foto2, foto3):
     menuPilihFoto = input("Masukan pilihan : ")
     if(menuPilihFoto == "1"):
         # untuk menampilkan foto ke layar
-        cv.imshow("Foto Bunga", foto1)
-        cv.waitKey()  # menunggu inputan untuk menutup windows foto
-        os.system("cls")  # membersihkan tampilan menu
+        # cv.imshow("Foto Bunga", foto1)
+        # cv.waitKey()  # menunggu inputan untuk menutup windows foto
+        # os.system("cls")  # membersihkan tampilan menu
         foto = foto1
     elif(menuPilihFoto == "2"):
         # untuk menampilkan foto ke layar
-        cv.imshow("Foto Rubik", foto2)
-        cv.waitKey()  # menunggu inputan untuk menutup windows foto
-        os.system("cls")  # membersihkan tampilan menu
+        # cv.imshow("Foto Rubik", foto2)
+        # cv.waitKey()  # menunggu inputan untuk menutup windows foto
+        # os.system("cls")  # membersihkan tampilan menu
         foto = foto2
     elif(menuPilihFoto == "3"):
         # untuk menampilkan foto ke layar
-        cv.imshow("Foto Rubik", foto3)
-        cv.waitKey()  # menunggu inputan untuk menutup windows foto
-        os.system("cls")  # membersihkan tampilan menu
+        # cv.imshow("Foto Rubik", foto3)
+        # cv.waitKey()  # menunggu inputan untuk menutup windows foto
+        # os.system("cls")  # membersihkan tampilan menu
         foto = foto3
     else:
         print("Pilihan tidak ada")
         input("Tekan apa saja untuk melanjutkan")
-        os.system("cls")
+        return foto
+    fotoTampil = cv.imread(foto+'.jpg' if foto == foto3 else foto+'.jpeg')
+    fotonih = cv.resize(fotoTampil, (600, 400)
+                        if foto == foto3 else (369, 492))
+    cv.imshow(foto, fotonih)
+    cv.waitKey(0)
     return foto
 
 
 def menuBins():
+    bins = 256
     print("""
     1. 256 Bins
     2. 32 Bins
@@ -87,63 +94,81 @@ def menuBins():
     """)
     menu1 = input("Masukan pilihan : ")
     if(menu1 == "1"):
-        pass
+        bins = 256
     elif(menu1 == "2"):
-        pass
+        bins = 32
     elif(menu1 == "3"):
-        pass
+        bins = 16
     elif(menu1 == "4"):
-        pass
+        bins = 8
     elif(menu1 == "5"):
-        pass
+        bins = 4
     else:
         print("Pilihan tidak ada")
         input("Tekan apa saja untuk melanjutkan")
+    return bins
 
 
-def menuHistogram():
+def menuHistogram(foto):
     print("""
-    1. Full histogram
-    2. Histogram per channel
+    1. Histogram Berwarna
+    2. Histogram GraySclae
     """)
     menu1 = input("Masukan pilihan : ")
     if(menu1 == "1"):
-        pass
-    elif(menu1 == "2"):
         print("""
-        1. Channel merah
-        2. Channel Hijau
-        3. Channel Biru
+        1. Full Channel
+        2. Red Channel 
+        3. Green Channel 
+        4. Blue Channel 
         """)
         menu2 = input("Masukan pilihan : ")
         if(menu2 == "1"):
-            pass
+            color = ('b', 'g', 'r')
+            plt.subplot(121)
+            plt.imshow(foto)
+            plt.subplot(122)
+            for i, col in enumerate(color):
+                histr = cv.calcHist([foto], [i], None, [256], [0, 256])
+                plt.plot(histr, color=col)
+                plt.xlim([0, 256])
+            plt.show()
         elif(menu2 == "2"):
             pass
         elif(menu2 == "3"):
             pass
+        elif(menu2 == "4"):
+            pass
         else:
             print("Pilihan tidak ada")
             input("Tekan apa saja untuk melanjutkan")
+    elif(menu1 == "2"):
+        fotoGrayscale = cv.cvtColor(foto, cv.COLOR_BGR2GRAY)
+        bins = menuBins()
+        plt.subplot(121)
+        plt.imshow(fotoGrayscale, 'gray')
+        plt.subplot(122)
+        plt.hist(fotoGrayscale.ravel(), bins, [0, 256])
+        plt.show()
     else:
         print("Pilihan tidak ada")
         input("Tekan apa saja untuk melanjutkan")
 
 
 def main():
-    fotoPemandangan = cv.imread("Foto pemandangan.jpeg")
-    fotoBunga = cv.imread("Foto bunga.jpeg")
-    fotoRubik = cv.imread("Foto rubik.jpg")
+    fotoPemandangan = "Foto pemandangan"
+    fotoBunga = "Foto bunga"
+    fotoRubik = "Foto rubik"
     dimension1 = (369, 492)  # ukuran baru foto bunga dan pemandangan
     dimension2 = (600, 400)  # ukuran baru foto rubik
     # untuk mengatur dimension/ukuran foto
-    foto1 = cv.resize(fotoPemandangan, dimension1)
-    foto2 = cv.resize(fotoBunga, dimension1)
-    foto3 = cv.resize(fotoRubik, dimension2)
-    foto = foto1
+    # foto1 = cv.resize(fotoPemandangan, dimension1)
+    # foto2 = cv.resize(fotoBunga, dimension1)
+    # foto3 = cv.resize(fotoRubik, dimension2)
+    foto = fotoPemandangan
     while(True):
         print("="*64)
-        print(">> Program Konversi Ruang Warna")
+        print(">> Program Pengolahan Citra")
         print("="*64)
         print("""
         1. Pilih foto
@@ -154,9 +179,13 @@ def main():
         print("="*64)
         menuUtama = input("Masukan pilihan : ")
         if(menuUtama == "1"):
-            foto = menuPilihFoto(foto1, foto2, foto3)
+            foto = menuPilihFoto(fotoPemandangan, fotoBunga, fotoRubik)
+            os.system("cls")
         elif(menuUtama == "2"):
             menuKonversiRuangWarna(foto)
+            os.system("cls")
+        elif(menuUtama == "3"):
+            menuHistogram(foto)
             os.system("cls")
         elif(menuUtama == "4"):
             print("Program Exit")
